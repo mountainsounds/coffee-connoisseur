271447b962d6268034dc1e3f090b25369d793e73
Zachary Sawyer <zsaw@Zacharys-iMac.local>
2022-01-16 07:36:48 -0500

    new project setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2021 Zack</p>
+      </footer>
+    </>
+  )
 }
 
 export default MyApp
